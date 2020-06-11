99d6e6fc8d50f29a68b3a0e5abb515be5184c678
zh-han-hub <66664824+zh-han-hub@users.noreply.github.com>
2020-06-11 17:13:10 +0800

    check_menu
    修复选取菜品时输入字符串而非数字时系统退出的问题

--- check_menu.sh
+++ check_menu.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 for var in $@ ; do
-    array1[${#array1[*]}]=$var
+	array1[${#array1[*]}]=$var
 done
-
-for(( i=1;i<${#array1[*]};i++)) do
-    array2[$i]=${array1[i]}
-done
-
-MAX=${array2[1]}
-for I in ${!array2[@]};do
-    if [[ ${MAX} -le ${array2[${I}]} ]];then
-        MAX=${array2[${I}]}
-    fi
-done
-if [ "${MAX}" -gt "${array1[0]}" ]
-then
-    echo 0
+a=1
+if [ ${#array1[*]} -eq $a ];then
+	echo 0
 else
-    echo 1
+	for(( i=0;i<${#array1[*]};i++)) do
+		if [ "${array1[i]}" -gt 0 ] 2>/dev/null ;then
+		s=`expr $s + 1`;
+		fi
+	done
+	b=`expr ${#array1[*]}`;
+	if [ $s -eq $b ];then
+		for(( i=1;i<${#array1[*]};i++)) do
+		array2[$i]=${array1[i]}
+		done
+		MAX=${array2[1]}
+		for I in ${!array2[@]};do
+			if [[ ${MAX} -le ${array2[${I}]} ]];then
+				MAX=${array2[${I}]}
+			fi
+		done
+		if [ "${MAX}" -gt "${array1[0]}" ];then
+			echo 0
+		else
+			echo 1
+		fi
+	else
+		echo 0
+	fi
 fi
